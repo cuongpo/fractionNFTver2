efa1b6700c56162359cbbadf410eb9410da12e03
cuongpham <61429471+cuongpo@users.noreply.github.com>
2023-05-31 16:43:10 +0700

    Update nft.sol

--- nft.sol
+++ nft.sol
@@ -11,19 +11,19 @@ contract nftcontract is ERC721, Ownable {
     
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    Counters.Counter private _collectionIds;
+    Counters.Counter private _realEstateIds;
     
     mapping (uint256 => string) public _tokenURIs;
     mapping (uint256 => string) public _tokenName;
-    mapping (uint256 => uint256) public _collectionId;
-    mapping (uint256 => string) public _collectionURIs;
-    mapping (uint256 => uint256) public _collectionThemeId;
-    mapping (uint256 => string) public _collectionName;
-    mapping (uint256 => address) public _collectionOwner;
+    mapping (uint256 => uint256) public _realEstateId;
+    mapping (uint256 => string) public _realEstateURIs;
+    mapping (uint256 => string) public _realEstateName;
+    mapping (uint256 => address) public _realEstateOwner;
     mapping (uint256 => uint256) public _tokenPower;
+    mapping (uint256 => uint256) public _numberOfFraction;
 
-    event MintNFT(uint256 tokenId, address recipient, string tokenURI,string name,uint256 collectionId_,uint256 tokenPower);
-    event CreateCollection(uint256 collectionId,string collectionName,string collectionURIs,uint256 collectionThemeId,address collectionOwner);
+    event MintNFT(uint256 tokenId, address recipient, string tokenURI,string name,uint256 realEstateId_,uint256 tokenPower);
+    event CreaterealEstate(uint256 realEstateId,string realEstateName,string realEstateURIs,uint256 _numberOfFraction,address realEstateOwner);
 
     string private _baseURIextended;
 
@@ -33,12 +33,12 @@ contract nftcontract is ERC721, Ownable {
         _baseURIextended = baseURI_;
     }
 
-    function mintNFT(address recipient, string memory tokenURI,string memory name,uint256 collectionId_,uint256 _power)
+    function mintNFT(address recipient, string memory tokenURI,string memory name,uint256 realEstateId_,uint256 _power)
         public
         returns (uint256)
     {
         require (
-            _collectionOwner[collectionId_] == msg.sender, 'not collection owner'
+            _realEstateOwner[realEstateId_] == msg.sender, 'not realEstate owner'
         );
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
@@ -47,8 +47,8 @@ contract nftcontract is ERC721, Ownable {
         _setTokenURI(newItemId, tokenURI);
         _setName(newItemId,name);
         _setPower(newItemId,_power);
-        _collectionId[newItemId] = collectionId_; 
-        emit MintNFT(newItemId,recipient,tokenURI,name,collectionId_,_power);
+        _realEstateId[newItemId] = realEstateId_; 
+        emit MintNFT(newItemId,recipient,tokenURI,name,realEstateId_,_power);
         return newItemId;
     }
 
@@ -80,25 +80,25 @@ contract nftcontract is ERC721, Ownable {
         return _baseURIextended;
     }
 
-    function createCollection(string memory collectionName_,string memory collectionURIs,uint256 collectionThemeId) public {
-        _collectionIds.increment();
-        uint256 newCollectionId = _collectionIds.current();
-        _collectionName[newCollectionId] = collectionName_;
-        _collectionOwner[newCollectionId] = msg.sender;
-        _collectionThemeId[newCollectionId] = collectionThemeId;
-        _collectionURIs[newCollectionId] = collectionURIs;
-        emit CreateCollection(newCollectionId,collectionName_,collectionURIs,collectionThemeId,msg.sender);
+    function createrealEstate(string memory realEstateName_,string memory realEstateURIs,uint256 numberOfFraction_) public {
+        _realEstateIds.increment();
+        uint256 newrealEstateId = _realEstateIds.current();
+        _realEstateName[newrealEstateId] = realEstateName_;
+        _realEstateOwner[newrealEstateId] = msg.sender;
+        _realEstateURIs[newrealEstateId] = realEstateURIs;
+        _numberOfFraction[newrealEstateId] = numberOfFraction_;
+        emit CreaterealEstate(newrealEstateId,realEstateName_,realEstateURIs,numberOfFraction_,msg.sender);
     }
 
     function getPower (uint256 tokenId) public view returns(uint256) {
         return _tokenPower[tokenId];
     }
 
-    function getCollectionId (uint256 tokenId) public view returns(uint256) {
-        return _collectionId[tokenId];
+    function getrealEstateId (uint256 tokenId) public view returns(uint256) {
+        return _realEstateId[tokenId];
     }
 
-    function getCollectionOwner (uint256 tokenId) public view returns(address) {
-        return _collectionOwner[tokenId];
+    function getrealEstateOwner (uint256 tokenId) public view returns(address) {
+        return _realEstateOwner[tokenId];
     }
 }
